e88b918b7d5f8c83ddcb314c8ba0034a8c921205
Mikael Vital <61027363+MikaelVital@users.noreply.github.com>
2022-12-06 09:21:59 -0300

    Atualizando Pré pago

--- Telefonia/src/PrePago.java
+++ Telefonia/src/PrePago.java
@@ -1,40 +1,51 @@
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class PrePago extends Assinante{
 	private float creditos;
 	private int numRecargas;
 	
-	public PrePago(long cpf, String nome, String numero, float creditos, int numRecargas) {
-		super(cpf, nome, numero);
+	private Recarga[] recargas;
+	
+	public PrePago(long cpf, String nome, int numero, float creditos, int numRecargas) {
+		super(cpf, nome, numero, numRecargas);
 		this.creditos = creditos;
 		this.numRecargas = numRecargas;
+		this.recargas = new Recarga[numRecargas];
+		Recarga[] recargas = new Recarga[numRecargas];
+
+	}
+
+	public void recarregar(GregorianCalendar data, float saldo) {
+		 if (numRecargas > 0) {
+	            this.recargas[this.numRecargas - 1] = new Recarga(data, saldo);
+	            this.numRecargas = this.numRecargas - 1;
+	            this.creditos += saldo;
+
+	            System.out.println("Recarga concluída. Seu saldo atual é de: R$" + Math.round(this.creditos * 100) / 100);
+	        } else {
+	            System.out.println("Recarga não realizada. Tente novamente.");
+	        }
 	}
 	
-	public float getCreditos() { 
-		return creditos; 
+	public void fazerChamada(Date data, int duracao) {
+		 if (numChamadas > 0) {
+	            float cobrança = creditos - (duracao * 1.45f);
+
+	            if (cobrança > 0) {
+	                this.creditos = cobrança;
+	                this.chamadas[this.numChamadas - 1] = new Chamada(data, duracao);
+	                this.numChamadas = this.numChamadas - 1;
+	                System.out.println("Chamada feita. Chamadas que você ainda possui: " + this.numChamadas + "Seu saldo atual é de: R$" + Math.round(this.creditos * 100) / 100);
+	            } else {
+	                System.out.println("Você não possui créditos suficientes para comletar está ligação. Por favor recarregue em uma de nossas centrais.");
+	            }
+	        } else {
+	            System.out.println("A chamada não pode ser realizada, tente novamente.");
+	        }
 	}
 	
-	public void setCreditos(float creditos) { 
-		this.creditos = creditos; 
-	}
-	
-	public int getNumRecargas() { 
-		return numRecargas; 
-	}
-	
-	public void setNumRecargas(int numRecargas) { 
-		this.numRecargas = numRecargas; 
-	}
-	
-	public void recarregar(Date data, float valor) { 
-		
-	}
-	
-	public void fazerChamada(Date data, int duracao) { 
-		
-	}
-	
-	public void imprimirFatura(int mes) { 
+	public void imprimirFatura(int mes) {
 		
 	}
 }

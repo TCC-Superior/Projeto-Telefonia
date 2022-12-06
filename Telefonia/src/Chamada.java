c44e7f3508dc742ee40dbd4c01f7c8637f300cd4
Mikael Vital <61027363+MikaelVital@users.noreply.github.com>
2022-12-06 09:20:07 -0300

    Atualizando Chamada

--- Telefonia/src/Chamada.java
+++ Telefonia/src/Chamada.java
@@ -6,15 +6,18 @@ public class Chamada {
 	private Date data;
 	private int duracao;
 
+	
 	public Chamada(Date date, int duracao) {
 		super();
 		this.data = date;
 		this.duracao = duracao;
 	}
 	
+
 	public Date getData() {
 		return data;
 	}
+
 
 	public int getDuracao() {
 		return duracao;
@@ -24,27 +27,28 @@ public class Chamada {
 		this.data = date;
 	}
 
+
 	public void setDuracao(int duracao) {
 		this.duracao = duracao;
 	}
     
 	@Override
-	public String toString() {
-		SimpleDateFormat formatacaoData = new SimpleDateFormat("dd-MM-yyyy");
+    public String toString() {
+    	SimpleDateFormat formatacaoData = new SimpleDateFormat("dd-MM-yyyy");
 		String dataFormatada = formatacaoData.format(data);
-		System.out.println("Data: " + dataFormatada + " Duração: " + duracao + " minutos.");
+        System.out.println("Data: " + dataFormatada + " Duração: " + duracao + " minutos.");
 		return "A chamada registrada é da data: " + dataFormatada + " e sua duração é de: " + duracao + " minutos.";
-	}
+  }
+	
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
 
-	public boolean equals(Object obj) {
-		if (this == obj)
-				return true;
-		if (obj == null)
-				return false;
-		if (getClass() != obj.getClass())
-				return false;
-
-		Chamada other = (Chamada) obj;
-		return Objects.equals(data, other.data) && Objects.equals(duracao, other.duracao);
-	}
+        Chamada other = (Chamada) obj;
+        return Objects.equals(data, other.data) && Objects.equals(duracao, other.duracao);
+    }
 }

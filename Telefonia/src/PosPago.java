d4414948a54cb520f306858ca9c699e6afe6e9f0
Mikael Vital <61027363+MikaelVital@users.noreply.github.com>
2022-12-06 09:21:27 -0300

    Atualizando Pós Pago

--- Telefonia/src/PosPago.java
+++ Telefonia/src/PosPago.java
@@ -1,4 +1,6 @@
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class PosPago extends Assinante{
 	private float assinatura;
@@ -16,29 +18,67 @@ public class PosPago extends Assinante{
 		this.assinatura = assinatura;
 	}
 	
+	
 	public void fazerChamada(Date date, int duracao) {
 		this.assinatura =  duracao * 1.04f; 
-		//this.chamadas.add(new Chamada(data, duracao));
 		
-		//if numChamadas > 0
+		if (numChamadas > 0) {
 		this.chamadas[this.numChamadas - 1] = new Chamada(date, duracao);
 		this.numChamadas = this.numChamadas - 1;
 		
-		System.out.println(this.numChamadas);
-	}
-	
-	public void mostrarChamadas() {
-		for(int i = 0; i < 2; i ++) {
-			System.out.println(this.chamadas[i]);
+		System.out.println("Chamada concluída. Você ainda possui " + this.numChamadas + " chamadas.");
+		} else {
+			System.out.println("Você não pode realizar uma chamada");
 		}
 	}
 	
-	public static void main(String[] args) {
-		PosPago p = new PosPago(1233211234, "Victoria", 1399112978, 2, 10f);
+	public void mostrarChamadas() {
+        int incremento = 0;
+        int contagem = 1;
+        //Verifica se o incremento é menor que o tamanho das chamadas
+        while (incremento < chamadas.length) {
+        	if (chamadas[incremento] == null) {
+        		incremento++; 
+        	} else {
+                		System.out.println(contagem + "chamada: " + this.chamadas[incremento]);
+                			incremento++;
+                			contagem++;
+                	}
+        	}
+		}
 	
-		p.fazerChamada(new Date(),10);
-		p.fazerChamada(new Date(),100);
+    public void imprimirFatura(int mes) {
+    	//Define o tipo do calendário
+        GregorianCalendar dataGC = new GregorianCalendar();
+        int incremento = 0;
+        float chamadasValor = 0;
+
+        System.out.println("Chamadas do plano Pós pago: ");
+        while (incremento < this.chamadas.length) {
+        	//Pega a chamada, verifica se é diferente de nulo e realiza a busca da chamada de acordo com o incremento e pega sua data
+            if (this.chamadas[incremento] != null) {
+            	dataGC.setTime(this.chamadas[incremento].getData());
+
+                int mesCal = dataGC.get(Calendar.MONTH);
+
+                if (mes == mesCal + 1) {
+
+                    int duracao = this.chamadas[incremento].getDuracao();
+                    //Definição dos valores da chamada
+                    chamadasValor += Math.round((duracao * 1.04f) * 100) / 100;
+
+                    System.out.println(this.chamadas[incremento].toString() + " Valor dessa chamada é de: R$" + Math.round((duracao * 1.04f) * 100) / 100);
+
+                    incremento++;
+                } else {
+                	incremento++;
+                }
+            } else {
+            	incremento++;
+            }
+        }
+        // Definição do valor total da assinatura
+        System.out.println("Preço total da assinatura é de: R$" + Math.round((this.assinatura * 100.0) / 100.0) + "E sua fatura total é de: R$" + Math.round((chamadasValor *100) / 100));
+    }
 		
-		p.mostrarChamadas();
 	}
-}
